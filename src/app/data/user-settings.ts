792ffb14c4821e49ef0e5efa485ed85c20fbcfbf
Affan Hakak <affanshowkat@gmail.com>
2022-07-15 18:31:24 +0530

    data binding added

---
+++ src/app/data/user-settings.ts
@@ -0,0 +1,9 @@
+export interface UserSettings {
+ name:string,
+ password:string,
+ emailOffers:boolean,
+ interfaceStyle:string,
+ subscriptionType:string,
+ notes:string,
+ date:string
+}
--- src/app/user-form/user-form.component.ts
+++ src/app/user-form/user-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserSettings } from '../data/user-settings';
 
 @Component({
   selector: 'app-user-form',
@@ -7,6 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserFormComponent implements OnInit {
 
+userSettings: UserSettings = {
+  name: '',
+  password: '',
+  emailOffers: false,
+  interfaceStyle: '',
+  subscriptionType: '',
+  notes: '',
+  date: ''
+}
   constructor() { }
 
   ngOnInit(): void {
