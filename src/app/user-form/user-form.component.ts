81c555b2b13e1124c63d324a67b667d796018175
Affan Hakak <affanshowkat@gmail.com>
2022-07-18 11:24:07 +0530

    added form validations on submit

--- src/app/user-form/user-form.component.ts
+++ src/app/user-form/user-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { UserSettings } from '../data/user-settings';
 
 @Component({
@@ -23,6 +24,10 @@ userSettings:UserSettings = {...this.actualUserSettings}
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(form:NgForm){
+console.log(form.valid)
   }
 
 }
