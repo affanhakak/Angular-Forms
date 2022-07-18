24a239a66813ef656cc3c527daf509cf45e20223
Affan Hakak <affanshowkat@gmail.com>
2022-07-18 19:11:21 +0530

    using http instead of fake observable

--- src/app/data/data.service.ts
+++ src/app/data/data.service.ts
@@ -1,14 +1,17 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { UserSettings } from './user-settings';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  constructor() { }
-  postUserSettingsForm(userSettings:UserSettings) : Observable<UserSettings>{
-    return of(userSettings);
+  constructor(private http:HttpClient) { }
+  postUserSettingsForm(userSettings:UserSettings) : Observable<any>{
+
+   return this.http.post('url', userSettings);
+   // return of(userSettings); // fake observable not needed anymore as we are now using http
   }
 }
